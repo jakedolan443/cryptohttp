dd84831662b4d64b63b7c9772966cf196995196c
jakedolan443 <jakedolan443@protonmail.com>
2021-08-31 00:48:34 +0100

    added files

---
+++ main.py
@@ -0,0 +1,16 @@
+import flask
+from server import Server
+
+
+app = flask.Flask(__name__, static_url_path='')
+@app.route("/")
+def serve():
+    data = server.get_data()
+    return flask.render_template("index.html", data=data)
+
+
+if __name__ == "__main__":
+    server = Server()
+    app.run()
+    
+    
