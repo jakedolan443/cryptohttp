51bd973b6597c55ef1c5cad02418337fdae60d65
jakedolan443 <jakedolan443@protonmail.com>
2021-08-31 11:24:06 +0100

    added argument functionality and simple setup script

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import flask
+import sys
 from server import Server
 
+
+help_menu = "USAGE: ./cryptohttp <ARGS>\n  --host <HOST>\t\thost to bind to, default 0.0.0.0\n  --port <PORT>\t\tport to bind to, default 8080\n  --refresh <RATE>\thow often to refresh API data, in seconds, default 3600"
+
+host = "0.0.0.0"
+port = 8080
+refresh_rate = 3600
+
+for entry in sys.argv:
+    try:
+        if entry == "--host":
+            host = sys.argv[sys.argv.index(entry)+1]
+        elif entry == "--port":
+            port = int(sys.argv[sys.argv.index(entry)+1])
+        elif entry == "--refresh":
+            refresh_rate = int(sys.argv[sys.argv.index(entry)+1])
+        elif entry == "--help":
+            print(help_menu)
+            exit()
+    except Exception:
+        print(help_menu)
+        exit()
 
 app = flask.Flask(__name__, static_url_path='')
 @app.route("/")
@@ -10,7 +32,7 @@ def serve():
 
 
 if __name__ == "__main__":
-    server = Server()
-    app.run("0.0.0.0", port=8080)
+    server = Server(refresh_rate)
+    app.run(host, port=port)
     
     
