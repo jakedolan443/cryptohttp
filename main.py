6ea8ae687050d3cccd4044466316bdfc1c96e60d
jcgoette <jcgoette@gmail.com>
2021-09-01 22:35:15 -0400

    add Docker functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import flask
+import os
 import sys
-from server import Server
 
+import flask
+
+from server import Server
 
 help_menu = "USAGE: ./cryptohttp <ARGS>\n  --host <HOST>\t\thost to bind to, default 0.0.0.0\n  --port <PORT>\t\tport to bind to, default 8080\n  --refresh <RATE>\thow often to refresh API data, in seconds, default 3600\n  --detach\t\trun cryptohttp in the background."
 
@@ -12,11 +14,11 @@ refresh_rate = 3600
 for entry in sys.argv:
     try:
         if entry == "--host":
-            host = sys.argv[sys.argv.index(entry)+1]
+            host = sys.argv[sys.argv.index(entry) + 1]
         elif entry == "--port":
-            port = int(sys.argv[sys.argv.index(entry)+1])
+            port = int(sys.argv[sys.argv.index(entry) + 1])
         elif entry == "--refresh":
-            refresh_rate = int(sys.argv[sys.argv.index(entry)+1])
+            refresh_rate = int(sys.argv[sys.argv.index(entry) + 1])
         elif entry == "--help":
             print(help_menu)
             exit()
@@ -24,7 +26,16 @@ for entry in sys.argv:
         print(help_menu)
         exit()
 
-app = flask.Flask(__name__, static_url_path='')
+if os.getenv("HOST"):
+    host = os.getenv("HOST")
+if os.getenv("PORT"):
+    port = int(os.getenv("PORT"))
+if os.getenv("REFRESH"):
+    refresh_rate = int(os.getenv("REFRESH"))
+
+app = flask.Flask(__name__, static_url_path="")
+
+
 @app.route("/")
 def serve():
     data = server.get_data()
@@ -34,5 +45,3 @@ def serve():
 if __name__ == "__main__":
     server = Server(refresh_rate)
     app.run(host, port=port)
-    
-    
